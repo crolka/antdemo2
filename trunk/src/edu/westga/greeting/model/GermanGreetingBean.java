4ca8f36c94d1b8a3ffa69e04359b8c89820f1287
crolka@hotmail.com <crolka@hotmail.com@58387b30-9a4a-11dd-8e58-870c635edf7a>
2008-10-21 21:27:52 +0000

    Added the GermanGreetingBean and SpanishGreetingBean to the repository.

---
+++ trunk/src/edu/westga/greeting/model/GermanGreetingBean.java
@@ -0,0 +1,31 @@
+package edu.westga.greeting.model;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class GermanGreetingBean implements IGreeting {
+
+	/**
+	 * Instantiates an EnglishGreetingBean object.
+	 */
+	public GermanGreetingBean() {
+	}
+
+	
+	/**
+	 * @see edu.westga.greeting.model.IGreeting#getGreeting()
+	 */
+	public String getGreeting() {
+		Calendar myCal = new GregorianCalendar();
+		int hour = myCal.get(Calendar.HOUR_OF_DAY);
+		String timeDay = " Tag";
+		
+		if (hour > 16){
+			timeDay = " Abend";
+		} else if (hour <= 12){
+			timeDay = " Morgen";
+		}
+		
+		return "Guten" + timeDay + ".  Wie geht es Ihnen?";
+	}
+}
